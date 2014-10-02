d8249b2eb0d9c6125b3f6285eaf6550207c6d135
Lexandar <zl359@cornell.edu>
2014-10-02 12:32:09 -0400

    nutriPhoneLib added

---
+++ nutriPhone/nutriPhoneLib/nutriPhoneLib/nutriPhoneLib/nutriPhoneLib.h
@@ -0,0 +1,19 @@
+//
+//  nutriPhoneLib.h
+//  nutriPhoneLib
+//
+//  Created by NutriPhone on 10/2/14.
+//  Copyright (c) 2014 EricksonLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for nutriPhoneLib.
+FOUNDATION_EXPORT double nutriPhoneLibVersionNumber;
+
+//! Project version string for nutriPhoneLib.
+FOUNDATION_EXPORT const unsigned char nutriPhoneLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <nutriPhoneLib/PublicHeader.h>
+
+
